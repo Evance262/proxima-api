0358059fcc6e51a5a15465375b28434587728f4e
Evance262 <evancechapuma62@gmail.com>
2022-09-07 16:04:43 +0200

    card views

--- src/core/views.py
+++ src/core/views.py
@@ -6,31 +6,36 @@ from rest_framework.views import APIView
 from .serializers import BankAccountSerializer, CreditCardSerializer
 from .models import BankAccount, CreditCard
 
-class CardView(APIView):
+class BankAccountView(APIView):
     def get(self, request, *args, **kwargs):
         bankQs = BankAccount.objects.all()
-        creditQs = CreditCard.objects.all()
 
         serializer1 = BankAccountSerializer(bankQs,many=True)
-        serializer2 = CreditCardSerializer(creditQs, many=True)
 
-        if serializer1:
-            return Response(serializer1.data)
-        elif serializer2:
-            return Response(serializer2.data)
-
+        return Response(serializer1.data)
 
     def post(self, request, *args, **kwargs):
         bank_serializer = BankAccountSerializer(data=request.data)
-        credit_serializer = CreditCardSerializer(data=request.data)
-
+       
         if bank_serializer.is_valid():
             bank_serializer.save()
             return Response(bank_serializer.data)
-        elif credit_serializer.is_valid():
-            credit_serializer.save()
-            return Response(credit_serializer.data)
-        else:
-            return Response(credit_serializer.errors)
 
         return Response(bank_serializer.errors)
+
+class CreditCardView(APIView):
+    def get(self, request, *args, **kwargs):
+        creditQs = CreditCard.objects.all()
+        serializer2 = CreditCardSerializer(creditQs, many=True)
+
+        return Response(serializer2.data)
+
+
+    def post(self, request, *args, **kwargs):
+        credit_serializer = CreditCardSerializer(data=request.data)
+
+        if credit_serializer.is_valid():
+            credit_serializer.save()
+            return Response(credit_serializer.data)
+
+        return Response(credit_serializer.errors)
--- src/proxima_api/urls.py
+++ src/proxima_api/urls.py
@@ -3,11 +3,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import CardView
+from core.views import CreditCardView, BankAccountView
 
 urlpatterns = [
     path('api-auth/',
          include('rest_framework.urls',)),
     path('admin/', admin.site.urls),
-    path('', CardView.as_view(), name='test')
+    path('credit_card/', CreditCardView.as_view(), name='credit_card'),
+    path('bank_account/', BankAccountView.as_view(), name='bank_account')
 ]
